5dbde7a9fc95a61cedd5795ecb1ecf0d093535f0
Ranilson de Andrade Saraiva Júnior <ranilsonjunior2@gmail.com>
2022-01-09 10:39:47 -0300

    teste final

--- index.js
+++ index.js
@@ -18,10 +18,200 @@ function palavra2() {
 
 function palavra3() {
     setTimeout(function() {
-        console.log('janela');
+        console.log('espelho');
     }, rand());
 }
+
+function palavra4() {
+    setTimeout(function() {
+        console.log('teto');
+    }, rand());
+}
+
+function palavra5() {
+    setTimeout(function() {
+        console.log('cadeira');
+    }, rand());
+}
+
+function palavra6() {
+    setTimeout(function() {
+        console.log('quarto');
+    }, rand());
+}
+
+function palavra7() {
+    setTimeout(function() {
+        console.log('prateleira');
+    }, rand());
+}
+
+function palavra8() {
+    setTimeout(function() {
+        console.log('cama');
+    }, rand());
+}
+
+function palavra9() {
+    setTimeout(function() {
+        console.log('parede');
+    }, rand());
+}
+
+function palavra10() {
+    setTimeout(function() {
+        console.log('torneira');
+    }, rand());
+}
+
+function palavra11() {
+    setTimeout(function() {
+        console.log('piso');
+    }, rand());
+}
+
+function palavra12() {
+    setTimeout(function() {
+        console.log('garagem');
+    }, rand());
+}
+
+function palavra13() {
+    setTimeout(function() {
+        console.log('pia');
+    }, rand());
+}
+
+function palavra14() {
+    setTimeout(function() {
+        console.log('assento sanitario');
+    }, rand());
+}
+
+function palavra15() {
+    setTimeout(function() {
+        console.log('quadro');
+    }, rand());
+}
+
+function palavra16() {
+    setTimeout(function() {
+        console.log('armário ou guarda-roupa');
+    }, rand());
+}
+
+function palavra17() {
+    setTimeout(function() {
+        console.log('mistura');
+    }, rand());
+}
+
+function palavra18() {
+    setTimeout(function() {
+        console.log('sala');
+    }, rand());
+}
+
+function palavra19() {
+    setTimeout(function() {
+        console.log('mau');
+    }, rand());
+}
+
+function palavra20() {
+    setTimeout(function() {
+        console.log('fantastico');
+    }, rand());
+}
+
+function palavra21() {
+    setTimeout(function() {
+        console.log('sofá');
+    }, rand());
+}
+
+function palavra22() {
+    setTimeout(function() {
+        console.log('gaveta');
+    }, rand());
+}
+
+function palavra23() {
+    setTimeout(function() {
+        console.log('relogio');
+    }, rand());
+}
+
+function palavra24() {
+    setTimeout(function() {
+        console.log('azulejo');
+    }, rand());
+}
+
+function palavra25() {
+    setTimeout(function() {
+        console.log('mesa');
+    }, rand());
+}
+
+function palavra26() {
+    setTimeout(function() {
+        console.log('forno micro-ondas');
+    }, rand());
+}
+
+function palavra27() {
+    setTimeout(function() {
+        console.log('correr');
+    }, rand());
+}
+
+function palavra28() {
+    setTimeout(function() {
+        console.log('cat');
+    }, rand());
+}
+
+function palavra29() {
+    setTimeout(function() {
+        console.log('dog');
+    }, rand());
+}
+
+function palavra30() {
+    setTimeout(function() {
+        console.log('peixe');
+    }, rand());
+}
+
 
 palavra1();
 palavra2();
 palavra3();
+palavra4();
+palavra5();
+palavra6();
+palavra7();
+palavra8();
+palavra9();
+palavra10();
+palavra11();
+palavra12();
+palavra13();
+palavra14();
+palavra15();
+palavra16();
+palavra17();
+palavra18();
+palavra19();
+palavra20();
+palavra21();
+palavra22();
+palavra23();
+palavra24();
+palavra25();
+palavra26();
+palavra27();
+palavra28();
+palavra29();
+palavra30();
